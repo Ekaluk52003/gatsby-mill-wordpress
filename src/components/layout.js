b45bc879d6d19186e3021a7f92c36ef0dba48847
Mill <ekaluk.p@gmail.com>
2019-05-29 23:50:24 +0700

    Day 3 tutorial

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,31 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from 'react'
+import MainMenu from './MainMenu'
+import Header from './header'
+import styled, {createGlobalStyle} from 'styled-components';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+const GlobalStyles  = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap');
+body {
+  font-family :'Roboto Mono', monospace;
+  margin : 0;
+  padding :0;
 }
+`
+const LayoutWrapper = styled.div`
+max-width : 960px;
+margin :0 auto;`
 
-export default Layout
+
+
+const Layout = ({children}) => (
+  <div>
+    <GlobalStyles />
+    <MainMenu />
+    <LayoutWrapper>
+     {children}  
+    </LayoutWrapper>
+   
+  </div>
+ )
+
+export default Layout;
